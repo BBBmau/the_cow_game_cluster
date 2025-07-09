fe1e3c936001b4fa197b65d1927aaea3925f5b8b
alvarez.mauriciotm@gmail.com <alvarez.mauriciotm@gmail.com>
2025-07-09 12:35:03 -0700

    initial tf config for thecowgame cluster creation

---
+++ main.tf
@@ -0,0 +1,14 @@
+  data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+module "kubernetes-engine_example_simple_autopilot_public" {
+  source  = "terraform-google-modules/kubernetes-engine/google//examples/simple_autopilot_public"
+  version = "37.0.0"
+  project_id = var.project_id
+  region = var.region
+}
